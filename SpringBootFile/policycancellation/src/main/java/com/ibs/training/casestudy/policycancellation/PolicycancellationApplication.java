5d27a949bc26605b5aa76b6cad0621a445b11b65
Elsa Maria <elsamariageorge@gmail.com>
2021-07-07 10:18:14 +0530

    First Commit of Spring Boot

---
+++ SpringBootFile/policycancellation/src/main/java/com/ibs/training/casestudy/policycancellation/PolicycancellationApplication.java
@@ -0,0 +1,13 @@
+package com.ibs.training.casestudy.policycancellation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicycancellationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicycancellationApplication.class, args);
+	}
+
+}
